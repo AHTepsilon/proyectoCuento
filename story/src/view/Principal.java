d20f9ddd7be38b2184682886f5bb169368307147
AHT <alehertoro725@gmail.com>
2021-04-12 11:58:14 -0500

    added the paralax effect

--- story/src/view/Principal.java
+++ story/src/view/Principal.java
@@ -16,16 +16,22 @@ public class Principal extends PApplet
 		size(1400, 800);
 	}
 	
-	PImage startScreen, backGround, gameScreen, endScreen;
+	PImage startScreen, backGround, gameScreen, endScreen, paralax, charAndText, sky;
 	boolean firstScene, secondScene, thirdScene;
+	int paralaxX;
 	
 	@Override
 	public void setup() //void Start
 	{
 		startScreen = loadImage("backGround.png");
 		gameScreen = loadImage("game screen.png");
+		paralax = loadImage("paralax.png");
+		charAndText = loadImage("characterANDtext.png");
+		sky = loadImage("sky.png");
 		
 		firstScene = true;
+		
+		paralaxX = -40;
 	}
 	
 	@Override
@@ -51,7 +57,12 @@ public class Principal extends PApplet
 		
 		if(secondScene)
 		{
+			paralaxX = (mouseX - 1400) / 15;
+			
+			image(sky, 0, 0);
+			image(paralax, paralaxX, 0);
 			image(gameScreen, 0, 0);
+			image(charAndText, 0, 0);
 			firstScene = false;
 		}
 	}
